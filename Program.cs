84e46fb979433df0d12a915f3458bec3460a9a08
Adam Vít <vita24@spstrutnov.cz>
2025-09-16 11:16:40 +0200

    jhfál

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace KamenNuzkyPapir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            nigger
+                dasdasd
+                ds
+                ds
+                ds
+                ds
+                ds
+
+                ds
+                ds
+                sd
+                sd
+                ds
+                sd
+                sd
+
+        }
+    }
+}
